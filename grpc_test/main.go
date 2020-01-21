95fe054557dc81947c075c4267f9e552b3f2813b
sora_kikuchi_xa <sora_kikuchi_xa@cyberagent.co.jp>
2020-01-21 22:23:02 +0900

    test wire

--- grpc_test/main.go
+++ grpc_test/main.go
@@ -1,1 +1,69 @@
-package grpc_test
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+
+	pb "github.com/skdrums/wire_test/tf_grpc/proto/tensorflow/serving"
+	tf_core_framework "github.com/tensorflow/tensorflow/tensorflow/go/core/framework"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	servingAddress := flag.String("serving-address", "localhost:10000", "The tensorflow serving address")
+	flag.Parse()
+
+	if flag.NArg() != 1 {
+		fmt.Println("Usage: " + os.Args[0] + " --serving-address localhost:10000 path/to/img.png")
+		os.Exit(1)
+	}
+
+	imgPath, err := filepath.Abs(flag.Arg(0))
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	imageBytes, err := ioutil.ReadFile(imgPath)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	request := &pb.PredictRequest{
+		ModelSpec: &pb.ModelSpec{
+			Name: "half_plus_two",
+		},
+		Inputs: map[string]*tf_core_framework.TensorProto{
+			"images": &tf_core_framework.TensorProto{
+				Dtype: tf_core_framework.DataType_DT_STRING,
+				TensorShape: &tf_core_framework.TensorShapeProto{
+					Dim: []*tf_core_framework.TensorShapeProto_Dim{
+						&tf_core_framework.TensorShapeProto_Dim{
+							Size: int64(1),
+						},
+					},
+				},
+				StringVal: [][]byte{imageBytes},
+			},
+		},
+	}
+
+	conn, err := grpc.Dial(*servingAddress, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("Cannot connect to the grpc server: %v\n", err)
+	}
+	defer conn.Close()
+
+	client := pb.NewPredictionServiceClient(conn)
+
+	resp, err := client.Predict(context.Background(), request)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	log.Println(resp)
+}
