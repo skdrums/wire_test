76f5f99dec02ea9a8c3de8c59c27d5628593ea9a
sora_kikuchi_xa <sora_kikuchi_xa@cyberagent.co.jp>
2020-01-29 17:24:30 +0900

    create new go file to control directory file

---
+++ test_files/shell/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"path/filepath"
+)
+
+type FilePath struct {
+	Filepath string
+}
+
+func main() {
+	workDir, _ := filepath.Abs("../annotation_tool_test")
+
+	filePaths := ReadAllFiles(workDir)
+	for _, path := range filePaths {
+		fmt.Println(path)
+	}
+}
+
+func ReadAllFiles(dir string) []string {
+	var paths []string
+	ob, _ := ioutil.ReadDir(dir)
+	for _, o := range ob {
+		path := filepath.Join(dir, o.Name())
+		if !o.IsDir() {
+			paths = append(paths, path)
+			continue
+		}
+		paths = append(paths, ReadAllFiles(path)...)
+	}
+
+	return paths
+}
